825612415b50c92da8bada9059d5f83ea8b7a19b
Maxi Lopez <maxilopezdiaz@live.com>
2022-08-28 19:30:03 -0300

    solicitud de nombre y dni (input)

---
+++ ejercico 3 input nomre y dni.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Usuario
+#
+# Created:     28/08/2022
+# Copyright:   (c) Usuario 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+nombre = input ("ingrese su nombre")
+dni = input ("ingrese su numero de DNI")
+print (f'El nombre del usuario es {nombre}')
+print(f'El numero de DNI es {dni}')
+
+if __name__ == '__main__':
+    main()
