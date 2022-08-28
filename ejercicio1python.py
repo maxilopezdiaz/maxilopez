b6af2804d1763937e26a63f8007bd0141f17084a
Maxi Lopez <maxilopezdiaz@live.com>
2022-08-28 18:56:42 -0300

    edad y altura maxi lopez

---
+++ ejercicio1python.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Usuario
+#
+# Created:     25/08/2022
+# Copyright:   (c) Usuario 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+maxilopez = 31
+altura = 180
+print (maxilopez)
+print (altura)
+if __name__ == '__main__':
+    main()
