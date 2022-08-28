11f699ca4769c58513d9db84b2b9e38c3e3c784b
Maxi Lopez <maxilopezdiaz@live.com>
2022-08-28 19:17:26 -0300

    ejercicio 2 concantenado nombre y apellido

---
+++ nombre y apellido concatenado.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module3
+# Purpose:
+#
+# Author:      Usuario
+#
+# Created:     28/08/2022
+# Copyright:   (c) Usuario 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+nombre = "Maximiliano "
+apellido = "Lopez"
+print (nombre + apellido)
+if __name__ == '__main__':
+    main()
