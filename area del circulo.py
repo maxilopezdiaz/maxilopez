36b271311e42a11d66bc2cbef99401e0079a8e21
Maxi Lopez <maxilopezdiaz@live.com>
2022-09-17 19:51:05 -0300

    area del circulo

---
+++ area del circulo.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      Usuario
+#
+# Created:     17/09/2022
+# Copyright:   (c) Usuario 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+def area_circulo (pi,radio):
+    return pi*radio**2
+print(area_circulo(3.14159,14))
+
+if __name__ == '__main__':
+    main()
