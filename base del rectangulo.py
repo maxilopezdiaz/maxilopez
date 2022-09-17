3d82fe13ba6e56ab31b26818e7a6ff22b98f4df9
Maxi Lopez <maxilopezdiaz@live.com>
2022-09-17 19:34:32 -0300

    imprimo area del rectangulo

---
+++ base del rectangulo.py
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      Usuario
+#
+# Created:     17/09/2022
+# Copyright:   (c) Usuario 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+def area_rectangulo (base,altura):
+
+    return base*altura
+
+print (area_rectangulo(6,2))
+
+if __name__ == '__main__':
+    main()
